5a29fb80a756919c938288bd7741df21b7b57463
maksym7576 <ukrainemaksymhryshchuk@gmail.com>
2025-03-12 12:17:12 +0100

    add tests for method getbookById

--- lib/book_service.dart
+++ lib/book_service.dart
@@ -10,14 +10,14 @@ class Book_service {
     return book;
   }
   Book? getBookById(int id) {
-    return _books.firstWhere(
+    return bookList.firstWhere(
           (book) => book.id == id,
       orElse: () => throw Exception("Book with id $id not found"),
     );
   }
 
   void updateBook(int id, Book updatedBook) {
-    int index = _books.indexWhere((book) => book.id == id);
-    _books[index] = updatedBook;
+    int index = bookList.indexWhere((book) => book.id == id);
+    bookList[index] = updatedBook;
   }
 }
--- test/book_service_test.dart
+++ test/book_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:library_crud/book.dart';
 import 'package:library_crud/book_service.dart';
 
 void main() {
@@ -17,6 +18,20 @@ void main() {
       final book2 = bookService.createBookAuto('Book2', 'Author2', 'Description2', 2002);
       expect(book2.id, equals(2));
       expect(bookService.bookList.length, equals(2));
+    });
+
+    test('getBookById should throw an exception if the book is not found', () {
+      expect(() => bookService.getBookById(999), throwsException);
+    });
+
+    test('getBookById should return an object', () {
+      bookService.createBookAuto('Book1', 'Author1', 'Description1', 2001);
+      Book? book = bookService.getBookById(1);
+      expect(book?.id, equals(1));
+      expect(book?.name, equals("Book1"));
+      expect(book?.author, equals("Author1"));
+      expect(book?.description, equals("Description1"));
+      expect(book?.yearPublished, equals(2001));
     });
   });
 }
