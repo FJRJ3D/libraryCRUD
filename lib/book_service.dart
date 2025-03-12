db3c819921aac22d01342e86c21ade698bbf72f0
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-03-12 11:04:46 +0100

    Delete method added.

--- lib/book_service.dart
+++ lib/book_service.dart
@@ -1,4 +1,13 @@
 
 class Book_service {
+  String deleteBookById(int id) {
+    int initialLength = bookList.length;
+    bookList.removeWhere((book) => book.id == id);
 
+    if(bookList.length < initialLength){
+      return "Book deleted.";
+    }else{
+      return "Book not deleted.";
+    }
+  }
 }
